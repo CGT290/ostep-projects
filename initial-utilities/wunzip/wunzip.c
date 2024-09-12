3898aae46c7e85929e73083fd39c57820c96efca
Peter-J1 <peterjoseph571@gmail.com>
2024-09-11 23:42:38 -0400

    wunzip finished

--- initial-utilities/wunzip/wunzip.c
+++ initial-utilities/wunzip/wunzip.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]){
            
         for(int k = 0; k < count; k++){
 
-                    putchar(c);
+                    printf("%c", c);
             }
 	    
       }
